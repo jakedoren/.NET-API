0504dfc8f34159a71fe095b647ddd5e2bdde4af8
Jacob Doren <jakedoren@gmail.com>
2021-08-29 12:07:17 -0400

    Items Controller created, get all Items functionality created

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,31 @@
+using System.Net;
+using System.Collections.Generic;
+using Catalog.Entities;
+using Catalog.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Controllers
+{
+    // GET /items
+
+    [ApiController]
+    [Route("items")]
+    public class ItemsController : ControllerBase
+    {
+        private readonly InMemItemsRepository repository;
+
+        public ItemsController()
+        {
+            repository = new InMemItemsRepository();
+        }
+
+        //GET /items
+
+        [HttpGet]
+        public IEnumerable<Item> getItems() 
+        {
+            var items = repository.GetItems();
+            return items;
+        }
+    }
+}
